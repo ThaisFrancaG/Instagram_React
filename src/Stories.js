a2a1c3266faff7502505d0a1b7f131b37171e97d
ThaisFrancaG <tfrancag@gmail.com>
2021-12-04 23:34:58 -0300

    stories está funcionando de um jeito mais razoável. Display ainda está troncho. Falta espaçamento e barra lateral

--- src/Stories.js
+++ src/Stories.js
@@ -3,39 +3,27 @@ export default function Stories(){
     //criando array com as fotos 
     const avatares = ["9gag","adorable_animals", "bad.vibes.memes","barked","catanacomics","nathanwpylestrangeplanet"];
 
-    const paths = avatares.map(avatar => `assets/img/${avatar}.svg`)
+    const paths = avatares.map(avatar => <div class = "story"><div class= "imagem"><img src = {`assets/img/${avatar}.svg`}/></div> <span class = "usuario">{avatar}</span></div>)
 
     const contasStories = {
         figura: paths,
         nome: avatares
     }
 
-    const {figura,nome} = contasStories;
+ 
+    const{ figura,nome} = contasStories;
+    console.log(figura);
+
 
 return(
-    <div class = "story">
-      
-       {paths.map(path => 
-       <div class = "story"> <img src = {path} alt = ":("/>
-       </div>)}
-       {avatares.map(avatar=> 
-       <div class = "story"> {avatar}
-       </div>)}
+    <div class = "stories">
+{paths}
+    
        
-      
-      
     </div>
 )
 }
 
 
-// function story(props){
-//     return(
-//         <div class = "story">
-
-
-//         </div>
-//     )
-// }
 
 
